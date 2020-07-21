b311c064f2aeedb3f43be665b8ee4a39ccaa50f6
rabbet35 <rabbet35@hotmail.com>
2020-07-21 21:04:14 +1000

    Initial Commit
    Started work on:
    -Vector3F: add and subtract overloads, two constructors
    
    -Vector4F: add and subtract overloads, two constructors

---
+++ MathClasses/Vector3F.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Vector3F
+    {
+        public float x, y, z;
+
+        public Vector3F(float x = 0, float y = 0, float z = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public Vector3F(float w = 0)
+        {
+            this.x = this.y = this.z = w;
+        }
+
+        public static Vector3F operator + (Vector3F a, Vector3F b)
+        {
+            return new Vector3F(a.x+b.x,a.y+b.y,a.z+b.z);
+        }
+        public static Vector3F operator - (Vector3F a, Vector3F b)
+        {
+            return new Vector3F(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+    }
+}
---
+++ MathClasses/Vector4F.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Vector4F
+    {
+        public float x, y, z, w;
+
+        public Vector4F(float x = 0, float y = 0, float z = 0, float w = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public Vector4F(float j = 0)
+        {
+            this.x = this.y = this.z = this.w = j; 
+        }
+
+        public static Vector4F operator + (Vector4F a, Vector4F b)
+        {
+            return new Vector4F(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+        }
+
+        public static Vector4F operator - (Vector4F a, Vector4F b)
+        {
+            return new Vector4F(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
+        }
+    }
+}
